3acec2e327aba42ea4d67d3d49d3087da0d2e5b4
VikkkeeSingh <vikkee.singh@clasicinformatics.com>
2020-08-09 09:25:16 +0530

    removed dist folder

--- dist/config/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    dbUrl: "mongodb://localhost:27017/",
-    bdName: "search-api",
-    jwtKey: "jsonwebtoken_secretKey",
-};
-//# sourceMappingURL=index.js.map
--- dist/middleware/checks.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.comparePassword = void 0;
-const bcryptjs_1 = require("bcryptjs");
-exports.comparePassword = (password, userPassword) => bcryptjs_1.compareSync(password, userPassword);
-//# sourceMappingURL=checks.js.map
